1e0a9a71a9fb502020efbd88d6fedba0d447d4c5
Tylko-jedno <diogkek@gmail.com>
2024-09-16 16:06:57 +0300

    first master commit

---
+++ Lab03.3/Lab03.3.cpp
@@ -0,0 +1,41 @@
+﻿// Lab_03_3.cpp
+// Головенко Костянтин
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 2
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x, R; //вхідні дані
+    double y;    //результат
+    
+    cout << "x = "; cin >> x;
+    cout << "R = "; cin >> R;
+
+    if (x <= -8)
+        y = -R;
+    else {
+        if (x <= -R)
+            y = (R * (R + x)) / (-R + 8);
+        else {
+            if (x <= R)
+                y = -sqrt((R - x) * (R + x));
+            else {
+                if (x <= 5)
+                    y = (2 * (x - R)) / (5 - R);
+                else
+                    y = 3;
+            }
+        }
+    }
+    
+    cout << "y = " << y << endl;
+
+}
+
+
